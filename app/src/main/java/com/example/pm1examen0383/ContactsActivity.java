9f89c55879aed6e6ce6ad4978fa1b34ee73cbdd0
henry <henryrosales@gmail.com>
2023-06-22 02:53:09 -0600

    examen primer parcial

---
+++ app/src/main/java/com/example/pm1examen0383/ContactsActivity.java
@@ -0,0 +1,178 @@
+package com.example.pm1examen0383;
+
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.pm1examen0383.adaptadores.ListaContactosAdapter;
+import com.example.pm1examen0383.db.DbContactos;
+import com.example.pm1examen0383.db.entidades.Contactos;
+
+import java.util.ArrayList;
+
+public class ContactsActivity extends AppCompatActivity {
+    private static final int REQUEST_CODE = 0;
+    Button btn_atras;
+    Button btn_actualizar;
+    Button btn_verimagen;
+    Button btn_eliminarimagen;
+    Button btn_compartircontacto;
+
+    RecyclerView listaContactos;
+    ArrayList<Contactos> listaArrayContactos;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contacts);
+
+        if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{(Manifest.permission.CALL_PHONE)},
+                    REQUEST_CODE);
+
+        }
+
+        btn_compartircontacto = findViewById(R.id.btncompartirContacto);
+        btn_compartircontacto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DbContactos dbContactos = new DbContactos(getApplicationContext());
+                ListaContactosAdapter lca = new ListaContactosAdapter();
+
+                if(lca.getId()==0){
+                    alerta("Seleccione un contacto",0);
+                }else{
+                    Intent sendIntent = new Intent();
+                    sendIntent.setAction(Intent.ACTION_SEND);
+                    String pais = dbContactos.verContactos(lca.getId()).getPais();
+
+                    boolean ciclo = true;
+                    String lada="";
+                    int c=1;
+                    while(ciclo==true){
+                        lada = lada + String.valueOf(pais.charAt(pais.length()-(c+1)));
+                        if(String.valueOf(pais.charAt(pais.length()-(c+2))).equals("(")){
+                            ciclo = false;
+                        }
+                        c++;
+                    }
+                    StringBuilder strb = new StringBuilder(lada);
+                    lada = strb.reverse().toString();
+                    System.out.println(lada);
+                    sendIntent.putExtra(Intent.EXTRA_TEXT, dbContactos.verContactos(lca.getId()).getNombre()+"\n"+lada+" "+dbContactos.verContactos(lca.getId()).getTelefono());
+                    sendIntent.setType("text/plain");
+
+                    Intent shareIntent = Intent.createChooser(sendIntent, null);
+                    startActivity(shareIntent);
+                }
+
+            }
+        });
+        //Se me fue el pajaro y le puse btn_eliminarimagen, debia llamarse btn_eliminarcontacto
+        btn_eliminarimagen = findViewById(R.id.btneliminarContacto);
+        btn_eliminarimagen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ListaContactosAdapter lca = new ListaContactosAdapter();
+                DbContactos dbContactos = new DbContactos(getApplicationContext());
+
+                if(lca.getId()==0){
+                    alerta("Seleccione un contacto",0);
+                }else{
+                    AlertDialog.Builder builder = new AlertDialog.Builder(ContactsActivity.this);
+                    builder.setMessage("¿Desea eliminar este contacto?")
+                            .setPositiveButton("SI", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    if(dbContactos.eliminarContacto(lca.getId())){
+                                        alerta("Eliminado con Éxito",0);
+                                        finish();
+                                        startActivity(getIntent());
+                                    }
+                                }
+                            })
+                            .setNegativeButton("NO", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+
+                                }
+                            }).show();
+                }
+            }
+        });
+
+        btn_verimagen = findViewById(R.id.btnverImagen);
+        btn_verimagen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ListaContactosAdapter lca = new ListaContactosAdapter();
+                Context context = view.getContext();
+
+                if(lca.getId()==0){
+                    alerta("Seleccione un contacto",0);
+                }else{
+                    Intent i = new Intent(context, ImagenActivity.class);
+                    i.putExtra("id", lca.getId());
+                    //System.out.println(lca.getId());
+                    context.startActivity(i);
+                }
+
+            }
+        });
+
+        btn_actualizar = findViewById(R.id.btnactualizarContacto);
+        btn_actualizar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ListaContactosAdapter lca = new ListaContactosAdapter();
+                Context context = view.getContext();
+
+                if(lca.getId()==0){
+                    alerta("Seleccione un contacto",0);
+                }else{
+                    //Intent i = new Intent(context, ViewsActivity.class);
+                    Intent i = new Intent(context, EditarActivity.class);
+                    i.putExtra("id", lca.getId());
+                    System.out.println(lca.getId());
+                    context.startActivity(i);
+                }
+            }
+        });
+
+        listaContactos = findViewById(R.id.rv_contactos);
+        listaContactos.setLayoutManager(new LinearLayoutManager(this));
+
+        DbContactos dbContactos = new DbContactos(ContactsActivity.this);
+
+        listaArrayContactos = new ArrayList<>();
+
+        ListaContactosAdapter adapter = new ListaContactosAdapter(dbContactos.mostrarContactos());
+        listaContactos.setAdapter(adapter);
+
+        btn_atras = findViewById(R.id.btn_atras);
+        btn_atras.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+
+    public void alerta(String a, int tipoduracion){
+        Toast toast1 = Toast.makeText(getApplicationContext(),a, tipoduracion);
+        toast1.show();
+    }
+}
---
+++ app/src/main/java/com/example/pm1examen0383/adaptadores/ListaContactosAdapter.java
@@ -0,0 +1,200 @@
+package com.example.pm1examen0383.adaptadores;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Color;
+import android.net.Uri;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.pm1examen0383.R;
+import com.example.pm1examen0383.db.DbContactos;
+import com.example.pm1examen0383.db.entidades.Contactos;
+
+import java.util.ArrayList;
+
+public class ListaContactosAdapter extends RecyclerView.Adapter<ListaContactosAdapter.ContactoViewHolder> {
+    private static int id;
+    ArrayList<Contactos> listaContactos;
+    View elmContacto[] = new  View[1000];
+    int c=-1;
+    int c2 = -1;
+    boolean wasPaso = false;
+
+    public ListaContactosAdapter(ArrayList<Contactos> listaContactos){
+        this.listaContactos = listaContactos;
+    }
+
+    public ListaContactosAdapter(){
+
+    }
+
+
+    public static int getId() {
+        return id;
+    }
+
+    public static void setId(int id) {
+        ListaContactosAdapter.id = id;
+    }
+
+    @NonNull
+    @Override
+    public ContactoViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.lista_item_contacto,null,false);
+        return new ContactoViewHolder(view);
+
+    }
+
+
+    @Override
+    public void onBindViewHolder(@NonNull ContactoViewHolder holder, int position) {
+        holder.viewPais.setText(listaContactos.get(position).getPais());
+        holder.viewNombre.setText(listaContactos.get(position).getNombre());
+        holder.viewTelefono.setText(listaContactos.get(position).getTelefono());
+        holder.viewNota.setText("Nota: "+listaContactos.get(position).getNota());
+    }
+
+    @Override
+    public int getItemCount() {
+        return listaContactos.size();
+    }
+
+    public class ContactoViewHolder extends RecyclerView.ViewHolder {
+        TextView viewPais, viewNombre, viewTelefono, viewNota;
+
+        public ContactoViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            //De momento el id de la clase es 0 porque ningun contacto esta selccionado al mostrar el RecyclerView
+            setId(0);
+
+            viewPais = itemView.findViewById(R.id.viewPais);
+            viewNombre = itemView.findViewById(R.id.viewNombre);
+            viewTelefono = itemView.findViewById(R.id.viewTelefono);
+            viewNota = itemView.findViewById(R.id.viewNota);
+
+            /*System.out.println("el ItemView es:"+itemView);
+            System.out.println(viewPais);
+            System.out.println(viewNombre);
+            System.out.println(viewTelefono);
+            System.out.println(viewNota);*/
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                //@RequiresApi(api = Build.VERSION_CODES.Q)
+                //@Override
+                public void onClick(View view) {
+                    /*System.out.println("----------------------------------------------------");
+                    System.out.println("el ItemView es:"+itemView);
+                    System.out.println(viewPais);
+                    System.out.println(viewNombre);
+                    System.out.println(viewTelefono);
+                    System.out.println(viewNota);
+                    System.out.println("--------------------------------------------------");
+                    System.out.println("----------------------------------------------------");
+                    System.out.println("el ItemView es:"+view);
+                    System.out.println(view);
+                    System.out.println(view);
+                    System.out.println(view);
+                    System.out.println(view);
+                    System.out.println("--------------------------------------------------");*/
+
+                    if(wasPaso==true){
+                        elmContacto[c].setBackgroundColor(Color.rgb(255,255,255));
+                        //El id es 0 lo que significa que ningun contacto esta siendo seleccionado
+                        setId(0);
+                    }
+                    wasPaso = true;
+                    c++;
+                    elmContacto[c] = itemView;
+
+                    itemView.setBackgroundColor(Color.rgb(149,188,200));
+                    setId(listaContactos.get(getAdapterPosition()).getId());
+
+                    //Si ya toco dos veces o más sobre uno o varios contacto
+                    if(c>0){
+                        //Si el contacto q toco es el mismo del toque anterior
+                        if(elmContacto[c]==elmContacto[c-1]){
+                            c2++;
+                            if(c2%2==0){
+                                elmContacto[c].setBackgroundColor(Color.rgb(255,255,255));
+                                //El id es 0 lo que significa que ningun contacto esta siendo seleccionado
+                                setId(0);
+                            }else{
+                                elmContacto[c].setBackgroundColor(Color.rgb(149,188,200));
+                                setId(listaContactos.get(getAdapterPosition()).getId());
+                            }
+
+                        }else {
+                            //Inicializar el contador que verifica los toques sobre un mismo contacto
+                            c2=-1;
+                        }
+                    }
+
+
+                    System.out.println(getId());
+                    //Revisar que el array no se llene
+                    System.out.println("TOQUES: "+c);
+                }
+            });
+
+            itemView.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View view) {
+
+                    Context context = view.getContext();
+
+                    AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                    builder.setMessage("¿Desea llamar a este contacto?")
+                            .setPositiveButton("SI", new DialogInterface.OnClickListener() {
+
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    ListaContactosAdapter lca = new ListaContactosAdapter();
+                                    DbContactos dbContactos = new DbContactos(context);
+                                    setId(listaContactos.get(getAdapterPosition()).getId());
+
+                                    Intent callintent = new Intent(Intent.ACTION_CALL);
+                                    //System.out.println(dbContactos.verContactos(lca.getId()).getTelefono());
+                                    String pais = dbContactos.verContactos(lca.getId()).getPais();
+
+                                    boolean ciclo = true;
+                                    String lada="";
+                                    int c=1;
+                                    while(ciclo==true){
+                                        lada = lada + String.valueOf(pais.charAt(pais.length()-(c+1)));
+                                        if(String.valueOf(pais.charAt(pais.length()-(c+2))).equals("(")){
+                                            ciclo = false;
+                                        }
+                                        c++;
+                                    }
+                                    StringBuilder strb = new StringBuilder(lada);
+                                    lada = strb.reverse().toString();
+                                    System.out.println(lada);
+                                    callintent.setData(Uri.parse("tel:"+lada+dbContactos.verContactos(lca.getId()).getTelefono()));
+                                    context.startActivity(callintent);
+
+
+
+
+                                }
+                            })
+                            .setNegativeButton("NO", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+
+                                }
+                            }).show();
+                    return false;
+                }
+            });
+        }
+    }
+}
